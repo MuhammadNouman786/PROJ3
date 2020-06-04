fbcdf652f10388aab3cb79da1304a6b4b8643130
Muhammad Nouman-786 <66272519+MuhammadNouman786@users.noreply.github.com>
2020-06-04 22:07:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World From Nouman
         </a>
       </header>
     </div>
